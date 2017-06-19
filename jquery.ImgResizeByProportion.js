67fd523b5c864cd44b7c879a4be04be546dddba7
not0000 <not0000@gmail.com>
2017-06-19 15:13:08 +0800

    Create jquery.ImgResizeByProportion.js

---
+++ jquery.ImgResizeByProportion.js
@@ -0,0 +1,13 @@
+//依照寬度當作基準，計算元素高度，放在resize事件裡就可以rwd了
+(function ($) {
+    $.fn.ImgResizeByProportion = function (TagClass, Proportion) {
+
+        wdth = $(window).width();
+        var mediaquery = [320, 480, 768, 992, 1200];
+        if (mediaquery.contain(wdth320) != -1) {
+            imgwidth = $(TagClass).width();
+            imgheight = imgwidth * Proportion;
+            $(TagClass).css("height", imgheight + "px");
+        }
+    }
+})(jQuery)
