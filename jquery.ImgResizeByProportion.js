c0d0f23a935e6ab70650f942e11eda412b6b1314
not0000 <not0000@gmail.com>
2017-06-19 16:07:05 +0800

    Update jquery.ImgResizeByProportion.js

--- jquery.ImgResizeByProportion.js
+++ jquery.ImgResizeByProportion.js
@@ -1,9 +1,7 @@
 (function ($) {
     //依照寬度當作基準，計算元素高度，放在resize事件裡呼叫就可以rwd了
     $.fn.ImgResizeByProportion = function (Proportion) {
-        wdth = $(window).width();
-        imgwidth = $(this).width();
-        imgheight = imgwidth * Proportion;
-        $(this).css("height", imgheight + "px");
+        tagheight = $(this).width() * Proportion;
+        $(this).css("height", tagheight + "px");
     }
 })(jQuery)
