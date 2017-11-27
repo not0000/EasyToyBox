1dfcd50cc9dac3a9f7fee0f2dd93291a7b5050b1
not <not0000@gmail.com>
2017-11-27 16:58:38 +0800

    修正頁面上style不會被列印的bug

--- printScreen.js
+++ printScreen.js
@@ -1,20 +1,20 @@
-//友善列印，自動抓列印頁面有用到的css
 function printScreen(printlist) {
-    var Rawlinkrel = document.getElementsByTagName("link");
+    var Rawlinkrel = document.querySelectorAll("link,style");
     var CombineLinkrel = "";
     for (var i = 0; i < Rawlinkrel.length; i++) {
         CombineLinkrel += String(Rawlinkrel[i].outerHTML);
     }
+	
     var value = document.getElementById(printlist).innerHTML;
-    console.log(CombineLinkrel);
-
     var printPage = window.open("", "printPage", "");
     printPage.document.open();
     printPage.document.write("<HTML><head></head><BODY onload='window.print();window.close()'>");
 
     printPage.document.write(CombineLinkrel);
-    printPage.document.write("<link href='/css/print.css' rel='stylesheet'>");
-
+    printPage.document.write("<link href='print.css' rel='stylesheet'>");
+    printPage.document.write("<div id='"+printlist+"'>");
     printPage.document.write(value);
+    printPage.document.write("</div>");
+
     printPage.document.close("</BODY></HTML>");
 }
